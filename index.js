ba4984b39af34fd26c5db887b65029ed5ce5c72c
A-amirul <amirulislamamir412@gmail.com>
2023-06-12 12:40:53 +0600

    make user as admin

--- index.js
+++ index.js
@@ -32,8 +32,31 @@ async function run() {
 		const myClassCollection = client.db("languageDb").collection("myClass");
 
 		// Users API
+		app.get('/users', async (req, res) => {
+			const result = await userCollection.find().toArray();
+			res.send(result);
+		})
+
+		app.patch('/users/admin/:id', async (req, res) => {
+			const id = req.params.id;
+			const filter = { _id: new ObjectId(id) };
+			const updateDoc = {
+				$set: {
+					role: 'admin'
+				}
+			};
+			const result = await userCollection.updateOne(filter, updateDoc);
+			res.send(result);
+		})
+
+
 		app.post('/users', async (req, res) => {
 			const user = req.body;
+			const query = { email: user.email }
+			const existingUser = await userCollection.findOne(query);
+			if (existingUser) {
+				return res.send({ message: 'user already exist' });
+			}
 			const result = await userCollection.insertOne(user);
 			res.send(result);
 
